298900d9d77a0e9ccf166b00629704b73b0f771a
Dian Sinta Sianturi <37674406+diansinta@users.noreply.github.com>
2019-05-30 14:20:19 +0700

    Add files via upload

---
+++ basic_coding.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 30, 2019 at 09:13 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `basic_coding`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_review`
+--
+
+CREATE TABLE `user_review` (
+  `id` int(11) NOT NULL,
+  `order_id` varchar(11) NOT NULL,
+  `product_id` varchar(11) NOT NULL,
+  `user_id` varchar(11) NOT NULL,
+  `rating` varchar(24) NOT NULL,
+  `review` varchar(255) NOT NULL,
+  `created_at` varchar(24) NOT NULL,
+  `update_at` varchar(24) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_review`
+--
+ALTER TABLE `user_review`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_review`
+--
+ALTER TABLE `user_review`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
